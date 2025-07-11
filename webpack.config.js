498fd782b8df91d1ae79b520a853f83a30e63f58
labcabrera <lab.cabrera@gmail.com>
2025-07-11 14:58:57 +0200

    Updated dev configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,15 @@ module.exports = (_, argv) => ({
   },
 
   devServer: {
+    allowedHosts: 'all',
+    host: '0.0.0.0',
     port: 8082,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    },
     historyApiFallback: true,
     watchFiles: [path.resolve(__dirname, "src")],
     onListening: function (devServer) {
