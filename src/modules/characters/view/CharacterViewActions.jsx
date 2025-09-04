1a3eec7292aca29a697d988a208f2455452de056
labcabrera <lab.cabrera@gmail.com>
2025-09-04 15:22:04 +0200

    Component refactor.

--- src/modules/characters/view/CharacterViewActions.jsx
+++ src/modules/characters/view/CharacterViewActions.jsx
@@ -1,24 +1,21 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import Box from '@mui/material/Box';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import { useError } from '../../../ErrorContext';
 import { deleteCharacter } from '../../api/characters';
+import DeleteDialog from '../../shared/dialogs/DeleteDialog';
 
 const CharacterViewActions = ({ character }) => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
   const { showError } = useError();
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
@@ -41,10 +38,6 @@ const CharacterViewActions = ({ character }) => {
     setDeleteDialogOpen(true);
   };
 
-  const handleDialogDeleteClose = () => {
-    setDeleteDialogOpen(false);
-  };
-
   const handleDialogDelete = () => {
     handleDelete();
     setDeleteDialogOpen(false);
@@ -59,13 +52,13 @@ const CharacterViewActions = ({ character }) => {
               Home
             </Link>
             <Link component={RouterLink} underline="hover" color="inherit" to="/strategic/games">
-              Strategic
+              {t('strategic')}
             </Link>
             <Link component={RouterLink} underline="hover" color="inherit" to="/strategic/characters">
-              Characters
+              {t('characters')}
             </Link>
             <span>{character.name}</span>
-            <span>View</span>
+            <span>{t('view')}</span>
           </Breadcrumbs>
         </Box>
         <Stack direction="row" spacing={2}>
@@ -77,23 +70,12 @@ const CharacterViewActions = ({ character }) => {
           </IconButton>
         </Stack>
       </Stack>
-      <Dialog
+      <DeleteDialog
+        message={`Are you sure you want to delete ${character.name} character? This action cannot be undone.`}
+        onDelete={() => handleDialogDelete()}
         open={deleteDialogOpen}
-        onClose={handleDialogDeleteClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{'Faction delete confirmation'}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Are you sure you want to remove {character.name}? This action cannot be undone
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDialogDeleteClose}>Cancel</Button>
-          <Button onClick={handleDialogDelete}>Delete</Button>
-        </DialogActions>
-      </Dialog>
+        onClose={() => setDeleteDialogOpen(false)}
+      />
     </>
   );
 };
--- src/modules/factions/view/FactionView.jsx
+++ src/modules/factions/view/FactionView.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
+import { useError } from '../../../ErrorContext';
 import { fetchCharacters } from '../../api/characters';
 import { fetchFaction } from '../../api/factions';
 import { fetchStrategicGame } from '../../api/strategic-games';
-import SnackbarError from '../../shared/errors/SnackbarError';
 import FactionViewActions from './FactionViewActions';
 import FactionViewAttributes from './FactionViewAttributes';
 import FactionViewCharacters from './FactionViewCharacters';
@@ -13,62 +13,64 @@ const FactionView = () => {
   const location = useLocation();
   const { factionId } = useParams();
   const [faction, setFaction] = useState(location.state?.faction || null);
-  const [strategicGame, setStrategicGame] = useState(null);
+  const [game, setGame] = useState(null);
   const [characters, setCharacters] = useState([]);
-  const [displayError, setDisplayError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const { showError } = useError();
 
-  const bindFaction = async (faction) => {
-    setFaction(await fetchFaction(factionId));
-    console.log('fetching game ' + faction.gameId);
-    await bindGame();
+  const bindFaction = (factionId) => {
+    fetchFaction(factionId)
+      .then((data) => {
+        setFaction(data);
+      })
+      .catch((error) => {
+        showError(error.message);
+      });
   };
 
-  const bindGame = async () => {
-    console.log('fetching game ' + faction.gameId);
-    const strategicGame = await fetchStrategicGame(faction.gameId);
-    setStrategicGame(strategicGame);
-    console.log('game ok ' + faction.gameId);
+  const bindGame = (gameId) => {
+    fetchStrategicGame(gameId)
+      .then((data) => {
+        setGame(data);
+      })
+      .catch((error) => {
+        showError(error.message);
+      });
   };
 
-  const bindCharacters = () => {
-    console.log('Binding characters for faction:', faction.id);
-    fetchCharacters(`factionId==${faction.id}`, 0, 100)
+  const bindCharacters = (factionId) => {
+    fetchCharacters(`factionId==${factionId}`, 0, 100)
       .then((data) => setCharacters(data))
       .catch((error) => {
-        setDisplayError(true);
-        setErrorMessage(`Error fetching characters: ${error.message}`);
+        showError(error.message);
       });
   };
 
   useEffect(() => {
     if (faction && faction.id) {
-      bindCharacters();
+      bindCharacters(faction.id);
+      bindGame(faction.gameId);
     }
   }, [faction]);
 
   useEffect(() => {
     if (!faction && factionId) {
-      bindFaction(faction);
-    } else {
-      bindGame();
+      bindFaction(factionId);
     }
   }, [faction, factionId]);
 
-  if (!faction || !strategicGame) return <div>Loading...</div>;
+  if (!faction || !game) return <div>Loading...</div>;
 
   return (
     <>
-      <FactionViewActions faction={faction} />
+      <FactionViewActions faction={faction} game={game} />
       <Grid container spacing={2}>
         <Grid size={6}>
-          <FactionViewAttributes faction={faction} setFaction={setFaction} strategicGame={strategicGame} />
+          <FactionViewAttributes faction={faction} setFaction={setFaction} strategicGame={game} />
         </Grid>
         <Grid size={6}>
           <FactionViewCharacters faction={faction} characters={characters} />
         </Grid>
       </Grid>
-      <SnackbarError open={displayError} message={errorMessage} onClose={() => setDisplayError(false)} />
     </>
   );
 };
