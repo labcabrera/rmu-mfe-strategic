5716ae7219f02a977c934637bfae067ffdae2df6
labcabrera <lab.cabrera@gmail.com>
2025-09-21 11:00:27 +0200

    Strategic game update.

---
+++ src/modules/games/update/StrategicGameUpdate.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Grid } from '@mui/material';
+import { StrategicGame, UpdateStrategicGameDto } from '../../api/strategic-game.dto';
+import StrategicGameAvatar from '../../shared/avatars/StrategicGameAvatar';
+import StrategicGameUpdateActions from './StrategicGameUpdateActions';
+import StrategicGameUpdateAttributes from './StrategicGameUpdateAttributes';
+import StrategicGameUpdateResume from './StrategicGameUpdateResume';
+
+const StrategicGameUpdate: FC = () => {
+  const location = useLocation();
+  const strategicGame = location.state?.strategicGame as StrategicGame;
+  const [formData, setFormData] = useState<UpdateStrategicGameDto>({
+    name: strategicGame.name,
+    description: strategicGame.description,
+    options: strategicGame.options,
+    powerLevel: strategicGame.powerLevel,
+  });
+
+  return (
+    <>
+      <StrategicGameUpdateActions strategicGame={strategicGame} formData={formData} />
+      <Grid container spacing={5}>
+        <Grid size={2}>
+          <StrategicGameAvatar strategicGame={strategicGame} size={300} />
+          <StrategicGameUpdateResume formData={formData} setFormData={setFormData} />
+        </Grid>
+        <Grid size={7}>
+          <StrategicGameUpdateAttributes formData={formData} setFormData={setFormData} />
+        </Grid>
+      </Grid>
+      <pre>{JSON.stringify(formData, null, 2)}</pre>
+    </>
+  );
+};
+
+export default StrategicGameUpdate;
---
+++ src/modules/games/update/StrategicGameUpdateAttributes.tsx
@@ -0,0 +1,146 @@
+import React, { Dispatch, FC, SetStateAction } from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { t } from 'i18next';
+import { UpdateStrategicGameDto } from '../../api/strategic-game.dto';
+import NumericInput from '../../shared/inputs/NumericInput';
+
+const StrategicGameUpdateAttributes: FC<{
+  formData: UpdateStrategicGameDto;
+  setFormData: Dispatch<SetStateAction<UpdateStrategicGameDto>>;
+}> = ({ formData, setFormData }) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleOptionsChange = (field: string, value: number) => {
+    setFormData({ ...formData, options: { ...formData.options, [field]: value } });
+  };
+
+  const handlePowerLevelChange = (field: string, value: number) => {
+    setFormData({ ...formData, powerLevel: { ...formData.powerLevel, [field]: value } });
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid size={12}>
+        <Typography variant="h6" color="primary">
+          {t('options')}
+        </Typography>
+      </Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('experience-multiplier')}
+          name="experienceMultiplier"
+          value={formData.options.experienceMultiplier}
+          onChange={(e) => handleOptionsChange('experienceMultiplier', e)}
+          allowNegatives={false}
+          min={0}
+          max={100}
+        />
+      </Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('fatigue-multiplier')}
+          name="fatigueMultiplier"
+          value={formData.options.fatigueMultiplier}
+          onChange={(e) => handleOptionsChange('fatigueMultiplier', e)}
+          allowNegatives={false}
+          min={0}
+          max={10}
+        />
+      </Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('board-scale-multiplier')}
+          name="boardScaleMultiplier"
+          value={formData.options.boardScaleMultiplier}
+          onChange={(e) => handleOptionsChange('boardScaleMultiplier', e)}
+          allowNegatives={false}
+          min={0}
+          max={100}
+        />
+      </Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('letality')}
+          name="letality"
+          value={formData.options.letality}
+          onChange={(e) => handleOptionsChange('letality', e)}
+          integer
+          min={-100}
+          max={100}
+        />
+      </Grid>
+      <Grid size={12}>
+        <Typography variant="h6" color="primary">
+          {t('power-level')}
+        </Typography>
+      </Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('stat-random-min')}
+          name="statRandomMin"
+          value={formData.powerLevel.statRandomMin}
+          onChange={(e) => handlePowerLevelChange('statRandomMin', e)}
+          integer
+          allowNegatives={false}
+          min={1}
+          max={100}
+        />
+      </Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('stat-boost-potential')}
+          name="statBoostPotential"
+          value={formData.powerLevel.statBoostPotential}
+          onChange={(e) => handlePowerLevelChange('statBoostPotential', e)}
+          integer
+          allowNegatives={false}
+          min={1}
+          max={100}
+        />
+      </Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('stat-boost-temporary')}
+          name="statBoostTemporary"
+          value={formData.powerLevel.statBoostTemporary}
+          onChange={(e) => handlePowerLevelChange('statBoostTemporary', e)}
+          integer
+          allowNegatives={false}
+          min={1}
+          max={100}
+        />
+      </Grid>
+      <Grid size={12}></Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('stat-creation-boosts')}
+          name="statCreationBoost"
+          value={formData.powerLevel.statCreationBoost}
+          onChange={(e) => handlePowerLevelChange('statCreationBoost', e)}
+          integer
+          allowNegatives={false}
+          min={0}
+          max={10}
+        />
+      </Grid>
+      <Grid size={3}>
+        <NumericInput
+          label={t('stat-creation-swaps')}
+          name="statCreationSwaps"
+          value={formData.powerLevel.statCreationSwap}
+          onChange={(e) => handlePowerLevelChange('statCreationSwap', e)}
+          integer
+          allowNegatives={false}
+          min={0}
+          max={10}
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default StrategicGameUpdateAttributes;
