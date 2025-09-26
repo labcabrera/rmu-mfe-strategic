8abb4082ab9e7d9d1e59be8c90f204b936291d2c
labcabrera <lab.cabrera@gmail.com>
2025-09-26 19:24:32 +0200

    Updated skill api client.

--- src/modules/api/skill.ts
+++ src/modules/api/skill.ts
@@ -2,10 +2,11 @@ import { buildErrorFromResponse } from './api-errors';
 import { Skill } from './skill.dto';
 
 export async function fetchSkills(): Promise<Skill[]> {
-  const url = `${process.env.RMU_API_CORE_URL}/skills`;
+  const url = `${process.env.RMU_API_CORE_URL}/skills?page=0&size=1000`;
   const response = await fetch(url, { method: 'GET' });
   if (response.status !== 200) {
     throw await buildErrorFromResponse(response, url);
   }
-  return (await response.json()) as Skill[];
+  const json = await response.json();
+  return json.content as Skill[];
 }
